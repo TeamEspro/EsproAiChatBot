8c1d3d3d39b329120b3799acaa4686f6d4497b63
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2025-10-02 21:04:31 +0530

    Update __init__.py

--- EsproAiChat/__init__.py
+++ EsproAiChat/__init__.py
@@ -1,4 +1,5 @@
 # This file makes EsproAiChat a Python package
 # You can optionally import main bot here if needed
 
-from .Espro import app  # Uncomment if you want to import bot app directly
+from .Espro import app
+from .start import app # Uncomment if you want to import bot app directly
