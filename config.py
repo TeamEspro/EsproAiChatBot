8f623f2612050cdb2901bfa3e1a514358e6626c6
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2025-10-02 20:16:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 ESPRO_API_KEY = os.environ.get("ESPRO_API_KEY")
 ESPRO_API_URL = os.environ.get(
     "ESPRO_API_URL",
-    "https://esproapi-9e00fce70d45.herokuapp.com/v1/chat/completions"
+    "https://esproapi-9e00fce70d45.herokuapp.com"
 )
 DEFAULT_MODEL = os.environ.get("DEFAULT_MODEL", "")
